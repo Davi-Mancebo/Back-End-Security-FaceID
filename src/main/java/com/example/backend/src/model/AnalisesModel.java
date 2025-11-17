96137e14dcc231aee64690033e80da7cbf5526f2
Davi Mancebo <87788044+Davi-Mancebo@users.noreply.github.com>
2025-11-17 14:42:42 -0300

    update in classes and paginations

--- src/main/java/com/example/backend/src/model/AnalisesModel.java
+++ src/main/java/com/example/backend/src/model/AnalisesModel.java
@@ -18,20 +18,15 @@ public class AnalisesModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String dispositivo;
 
-    @Column(nullable = false)
     private boolean status;
 
     @Lob
     @Column(nullable = false)
-    private byte[] imagem; // <-- imagem em BLOB
+    private byte[] imagem; // Armazena a foto como BLOB
 
-    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
-    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     @PrePersist
